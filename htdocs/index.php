5daae34dadc5ddd4858564a59557480972334347
Curtis Gedak <gedakc@gmail.com>
2009-12-02 09:37:59 -0700

    Update news and links for gparted 0.5.0 release

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,7 +17,7 @@ gnome_menu();
 ?>
 <div class="rightbox">
 	<b>Latest release:</b><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.4.8/">GParted 0.4.8</a><br>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.5.0/">GParted 0.5.0</a><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.4.8-6/">Live 0.4.8-6</a>
 	</div>
 <div class="content">
