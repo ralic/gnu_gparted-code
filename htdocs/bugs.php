1134490fa0272c305e278cba054cda030bcab3bd
Curtis Gedak <gedakc@gmail.com>
2009-12-22 13:54:39 -0700

    Add link for tips on reporting bugs

--- htdocs/bugs.php
+++ htdocs/bugs.php
@@ -38,9 +38,12 @@ Please file new bugs <a href="https://bugzilla.gnome.org/enter_bug.cgi?product=g
 <a href="http://bugzilla.gnome.org/buglist.cgi?query_format=advanced&amp;short_desc_type=allwordssubstr&amp;short_desc=&amp;classification=Other&amp;product=gparted&amp;component=docs&amp;long_desc_type=substring&amp;long_desc=&amp;status_whiteboard_type=allwordssubstr&amp;status_whiteboard=&amp;keywords_type=allwords&amp;keywords=&amp;bug_status=UNCONFIRMED&amp;bug_status=NEW&amp;bug_status=ASSIGNED&amp;bug_status=REOPENED&amp;bug_status=NEEDINFO&amp;bug_status=RESOLVED&amp;bug_status=VERIFIED&amp;bug_status=CLOSED&amp;emailassigned_to1=1&amp;emailtype1=substring&amp;email1=&amp;emailassigned_to2=1&amp;emailreporter2=1&amp;emailqa_contact2=1&amp;emailcc2=1&amp;emailtype2=substring&amp;email2=&amp;bugidtype=include&amp;bug_id=&amp;chfieldfrom=&amp;chfieldto=Now&amp;chfieldvalue=&amp;cmdtype=doit&amp;order=Reuse+same+sort+as+last+time&amp;field0-0-0=noop&amp;type0-0-0=noop&amp;value0-0-0=">all bugs</a>
 <br>
 <br><br>
+Tips on reporting bugs:
+<a href="http://www.chiark.greenend.org.uk/~sgtatham/bugs.html">How to Report Bugs Effectively</a><br>
+<br>
 More information about Gnome's bugzilla can be found at <a href="http://bugzilla.gnome.org/">http://bugzilla.gnome.org</a>
 <br>
-<br><br>
+<br>
 </div>
 
 <? gnome_foot() ?>
