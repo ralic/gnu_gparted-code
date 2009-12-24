538c06e5f17e554adf3494d0e29c2d1de18a84c5
Curtis Gedak <gedakc@gmail.com>
2009-12-24 09:54:50 -0700

    Add donation

--- htdocs/donations.php
+++ htdocs/donations.php
@@ -56,6 +56,11 @@ We are grateful for all donations.<br>
 	<TD><B>Amount</B></TD>
 </TR>
 <TR>
+	<TD>2009-12-24</TD>
+	<TD>anonymous</TD>
+	<TD>10 USD (- 0.69 USD to PP)</TD>
+</TR>
+<TR>
 	<TD>2009-12-16</TD>
 	<TD>Andreas Bäß</TD>
 	<TD>20 USD (- 1.08 USD to PP)</TD>
