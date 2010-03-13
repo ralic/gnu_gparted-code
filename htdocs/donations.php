f2c46fa881c5dbea086a2e1bde8d5720e07740ed
Curtis Gedak <gedakc@gmail.com>
2010-03-13 09:06:55 -0700

    Add donation

--- htdocs/donations.php
+++ htdocs/donations.php
@@ -56,6 +56,11 @@ We are grateful for all donations.<br>
 	<TD><B>Amount</B></TD>
 </TR>
 <TR>
+	<TD>2010-03-13</TD>
+	<TD>anonymous</TD>
+	<TD>10 USD (- 0.69 USD to PP)</TD>
+</TR>
+<TR>
 	<TD>2010-03-12</TD>
 	<TD>anonymous</TD>
 	<TD>5 USD (- 0.50 USD to PP)</TD>
