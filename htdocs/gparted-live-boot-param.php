1a5262cf0b2b7583681b5aa9a49e7066130bb091
Steven Shiau <steven@nchc.org.tw>
2010-08-31 19:21:25 +0800

    minor update

--- htdocs/gparted-live-boot-param.php
+++ htdocs/gparted-live-boot-param.php
@@ -1,4 +1,23 @@
-The boot parameters for GParted live
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<?
+include "functions.php";
+?>
+
+<head>
+  <title>GParted -- Live CD/USB/PXE/HD </title>
+<? html_head() ?>
+</head>
+<body>
+<div class="body">
+
+<?
+gnome_head();
+gnome_menu();
+?>
+<div class="content">
+<h1>The boot parameters for GParted live<h1>
+<p>
 GParted live is based on <a href="http://wiki.debian.org/DebianLive/" target=_blank>Debian live</a> with GParted installed. Therefore there are 2 kinds of boot parameters:
 <ol>
 <li>Boot parameters from live-initramfs. You can refer to this <a href="./live-initramfs.en.7.txt" target=_blank>manual of live-initramfs</a>.
