9095b11767721be79f7f832054dc9d08374ff796
Curtis Gedak <gedakc@gmail.com>
2010-10-15 14:09:45 -0600

    Reword text and cleanup html on artwork page

--- htdocs/artwork.php
+++ htdocs/artwork.php
@@ -19,125 +19,120 @@ gnome_menu();
 <div class="content">
 <h1>Artwork</h1>
 <p>
-Here you can find the icons people created for GParted with contactinfo of their designers. Please let me know if you dislike the idea of exposing your emailadress to the world, so i can leave it out.<br>
-Thanks!
+Here are the icons people created for GParted along with the contact
+information of each designer.
 </p>
 
 <table border=1>
 <tr>
-        <td>
-                <a href="images/gparted_wallpaper.png">
-                        <img src="images/gparted_wallpaper_small.png" alt="wallpaper1">
-                </a>
-        </td>
-	<td>
-		<a href="gparted_icons/gparted_logo1.png">
-			<img src="gparted_icons/gparted_logo1_small.png" alt="logo1">
-		</a>
-	</td>
-	<td>
-		<a href="gparted_icons/gparted_logo2.png">
-			<img src="gparted_icons/gparted_logo2_small.png" alt="logo2">
-		</a>
-	</td>
-	<td>
-		<a href="gparted_icons/gparted_logo3.png">
-			<img src="gparted_icons/gparted_logo3_small.png" alt="logo3">
-		</a>
-	</td>
+  <td>
+    <a href="images/gparted_wallpaper.png">
+      <img src="images/gparted_wallpaper_small.png" alt="wallpaper1">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo1.png">
+      <img src="gparted_icons/gparted_logo1_small.png" alt="logo1">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo2.png">
+      <img src="gparted_icons/gparted_logo2_small.png" alt="logo2">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo3.png">
+      <img src="gparted_icons/gparted_logo3_small.png" alt="logo3">
+    </a>
+  </td>
 </tr>
 <tr>
-        <td>
-                Created by:<br>
-                <a href="mailto:jlmujar&#64;trends.com.ph" >Junel Mujar</a><br>
-                <br>
-        </td>
-	<td>
-		Created by:<br>
-		<a href="mailto:info&#064;desing.nl" >blokhut</a><br>
-		<a href="http://www.desing.nl" target="new">http://www.desing.nl</a>
-	</td>
-	<td>
-		Created by:<br>
-		<a href="mailto:info&#064;desing.nl" >blokhut</a><br>
-		<a href="http://www.desing.nl" target="new">http://www.desing.nl</a>
-	</td>
-	<td>
-		Created by:<br>
-		<a href="mailto:josvandersteege&#064;hotmail.com" >Jos van der Steege</a><br>
-		<br>
-	</td>
+  <td>
+    Created by:<br>
+    <a href="mailto:jlmujar&#64;trends.com.ph">Junel Mujar</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:info&#064;desing.nl">blokhut</a><br>
+    <a href="http://www.desing.nl" target="new">http://www.desing.nl</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:info&#064;desing.nl">blokhut</a><br>
+    <a href="http://www.desing.nl" target="new">http://www.desing.nl</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:josvandersteege&#064;hotmail.com">Jos van der Steege</a>
+  </td>
 </tr>
 </table>
+
 <table border=1>
 <tr>
-        <td>
-                <a href="gparted_icons/gparted_logo4.png">
-                        <img src="gparted_icons/gparted_logo4.png" alt="logo4">
-                </a>
-        </td>
-        <td>
-                <a href="gparted_icons/gparted_logo5.png">
-                        <img src="gparted_icons/gparted_logo5.png" alt="logo5">
-                </a>
-        </td>
-        <td>
-                <a href="gparted_icons/gparted_logo6.png">
-                        <img src="gparted_icons/gparted_logo6.png" alt="logo6">
-                </a>
-        </td>
-        <td>
-                <a href="gparted_icons/gparted_logo7.png">
-                        <img src="gparted_icons/gparted_logo7.png" alt="logo7">
-                </a>
-        </td>
-        <td>
-                <a href="gparted_icons/gparted_logo8.png">
-                        <img src="gparted_icons/gparted_logo8.png" alt="logo8">
-                </a>
-        </td>
-        <td>
-                <a href="gparted_icons/gparted_logo9.png">
-                        <img src="gparted_icons/gparted_logo9.png" alt="logo9">
-                </a>
-        </td>
+  <td>
+    <a href="gparted_icons/gparted_logo4.png">
+      <img src="gparted_icons/gparted_logo4.png" alt="logo4">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo5.png">
+      <img src="gparted_icons/gparted_logo5.png" alt="logo5">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo6.png">
+      <img src="gparted_icons/gparted_logo6.png" alt="logo6">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo7.png">
+      <img src="gparted_icons/gparted_logo7.png" alt="logo7">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo8.png">
+      <img src="gparted_icons/gparted_logo8.png" alt="logo8">
+    </a>
+  </td>
+  <td>
+    <a href="gparted_icons/gparted_logo9.png">
+      <img src="gparted_icons/gparted_logo9.png" alt="logo9">
+    </a>
+  </td>
 </tr>
 <tr>
-        <td>
-                Created by:<br>
-                <a href="mailto:eloli&#064;hotmail.com" >Eugenia Loli-Queru</a><br>
-                <a href="" target="new"></a>
-        </td>
-        <td>
-                Created by:<br>
-                <a href="mailto:tim.fuchs&#064;gmail.com" >Tim Fuchs</a><br>
-                <a href="" target="new"></a>
-        </td>
-        <td>
-                Created by:<br>
-                <a href="mailto:marko_pinux&#064;hotmail.com" >Marko Sosic</a><br>
-                <a href="" target="new"></a>
-        </td>
-        <td>
-                Created by:<br>
-                <a href="mailto:marko_pinux&#064;hotmail.com" >Marko Sosic</a><br>
-                <a href="" target="new"></a>
-        </td>
-        <td>
-                Created by:<br>
-                <a href="mailto:ac&#064;crystalti.com" >Alain Clement</a><br>
-                <a href="" target="new"></a>
-        </td>
-        <td>
-                Created by:<br>
-                <a href="mailto:kraft.sebastian&#064;gmail.com" >Sebastian Kraft</a><br>
-                <a href="" target="new"></a>
-        </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:eloli&#064;hotmail.com">Eugenia Loli-Queru</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:tim.fuchs&#064;gmail.com">Tim Fuchs</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:marko_pinux&#064;hotmail.com">Marko Sosic</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:marko_pinux&#064;hotmail.com">Marko Sosic</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:ac&#064;crystalti.com">Alain Clement</a>
+  </td>
+  <td>
+    Created by:<br>
+    <a href="mailto:kraft.sebastian&#064;gmail.com">Sebastian Kraft</a>
+  </td>
 </tr>
 </table>
 
-<br><br><br>
-
+<p>
+Thanks goes to each of the designers for sharing these creations.
+</p>
+<br>
 
 </div>
 
