48f12131185a06f2e0331ca65311f11187e48247
Curtis Gedak <gedakc@gmail.com>
2010-10-23 10:35:46 -0600

    Reword and rearrange sections on development status page

--- htdocs/dev-status.php
+++ htdocs/dev-status.php
@@ -23,6 +23,28 @@ Work on the GParted application is managed by Curtis Gedak.<br>
 Work on the GParted Live image is managed by Steven Shiau.
 </p>
 
+<h2>Releases</h2>
+<p>
+Plans for new releases of GParted can be found in
+the <a href="http://gparted-forum.surf4.info/viewforum.php?id=4">General
+Development</a> forum.<br>
+<br>
+We strive to create new releases of GParted about every 2 months.
+Having said that, we will create a new release in a much shorter time
+frame, especially if needed to fix critical bugs.
+</p>
+
+<h2>Source Code Repository</h2>
+<p>
+The source code for GParted is managed under the Gnome git repository.
+You can <a href="http://git.gnome.org/cgit/gparted/">browse</a> the
+git repository visually, or read
+instructions <a href="http://live.gnome.org/Git/Developers">here</a>.<br>
+<br>
+For those trying to compile GParted from the git repository see:<br>
+<a href="http://gparted-forum.surf4.info/viewtopic.php?id=14136">How to Build GParted from git Source Code Repository</a>
+</p>
+
 <h2>Communication</h2>
 <p>
 The <a href="forum.php">forum</a> is used extensively for
@@ -38,28 +60,6 @@ and <a href="http://gparted-forum.surf4.info/viewforum.php?id=5">Feature
 Requests</a>.<br>
 <br>
 <a href="http://quad/gparted/htdocs/bugs.php">Bugs</a> are reported and tracked using the Gnome bugzilla bug tracking system.
-</p>
-
-<h2>Releases</h2>
-<p>
-We strive to create new releases of GParted at least every 2 or 3 months.
-Having said that, we will create a new release in a much shorter time
-frame, especially if needed to fix critical bugs.<br>
-<br>
-Plans for new releases of GParted can be found in
-the <a href="http://gparted-forum.surf4.info/viewforum.php?id=4">General
-Development</a> forum.
-</p>
-
-<h2>Source Code Repository</h2>
-<p>
-The source code for GParted is managed under the Gnome git repository.
-You can <a href="http://git.gnome.org/cgit/gparted/">browse</a> the
-git repository visually, or read
-instructions <a href="http://live.gnome.org/Git/Developers">here</a>.<br>
-<br>
-For those trying to compile GParted from the git repository see:<br>
-<a href="http://gparted-forum.surf4.info/viewtopic.php?id=14136">How to Build GParted from git Source Code Repository</a>
 </p>
 
 <h2>Application Development Guidelines</h2>
