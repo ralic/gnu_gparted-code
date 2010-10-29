2a522bc9332efe20bef2c57d8a5a3c1e9b53dde3
Curtis Gedak <gedakc@gmail.com>
2010-10-29 09:43:14 -0600

    Update news and links for gparted 0.7.0 release

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,7 +17,7 @@ gnome_menu();
 ?>
 <div class="rightbox">
 	<b>Latest release:</b><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.6.4/">GParted 0.6.4</a><br>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.7.0/">GParted 0.7.0</a><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.6.4-1/">Live 0.6.4-1</a>
 	</div>
 <div class="content">
