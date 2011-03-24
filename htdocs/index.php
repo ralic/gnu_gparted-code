2cd079307678403c55569f84b52b9d28727176e2
Steven Shiau <steven@nchc.org.tw>
2011-03-24 08:03:05 +0800

    Update GParted live latest release as 0.8.0-5.

--- htdocs/index.php
+++ htdocs/index.php
@@ -18,7 +18,7 @@ gnome_menu();
 <div class="rightbox">
 	<b>Latest release:</b><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.8.0/">GParted 0.8.0</a><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.8.0-1/">Live 0.8.0-1</a>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.8.0-5/">Live 0.8.0-5</a>
 	</div>
 <div class="content">
 
