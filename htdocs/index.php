2af43f5b851b0813892775617c0b2a4cafe10dcf
Curtis Gedak <gedakc@gmail.com>
2011-05-23 10:18:14 -0600

    Update news and links for gparted 0.8.1 release

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,7 +17,7 @@ gnome_menu();
 ?>
 <div class="rightbox">
 	<b>Latest release:</b><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.8.0/">GParted 0.8.0</a><br>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.8.1/">GParted 0.8.1</a><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.8.0-5/">Live 0.8.0-5</a>
 	</div>
 <div class="content">
