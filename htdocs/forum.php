0bd3e287cc1bebfa75d755878484a091be41acda
Curtis Gedak <gedakc@gmail.com>
2011-10-19 17:31:28 -0600

    Add forum specific links to forum page

--- htdocs/forum.php
+++ htdocs/forum.php
@@ -19,11 +19,31 @@ gnome_menu();
 <h1>Forum</h1>
 <p>
 Please remember that the forum is for questions and ideas!<br>
-Bugs go <a href="bugs.php">here</a>
+Bugs go <a href="bugs.php">here</a>.
 <br><br>
-Enter <a href="http://gparted-forum.surf4.info">forum</a>
+Enter <a href="http://gparted-forum.surf4.info">forum</a>.
+<br><br>
 </p>
-<br>
+
+<h2>User Support</h2>
+<p>
+User support is provided in
+the <a href="http://gparted-forum.surf4.info/viewforum.php?id=2">GParted</a>
+and <a href="http://gparted-forum.surf4.info/viewforum.php?id=3">Live
+Media</a> forums.
+<br><br>
+</p>
+
+<h2>Development</h2>
+<p>
+Development discussions take place in forums such
+as, <a href="http://gparted-forum.surf4.info/viewforum.php?id=4">General
+Development</a>,
+<a href="http://gparted-forum.surf4.info/viewforum.php?id=8">Testers</a>,
+and <a href="http://gparted-forum.surf4.info/viewforum.php?id=5">Feature
+Requests</a>.
+</p>
+
 </div>
 
 <? gnome_foot() ?>
