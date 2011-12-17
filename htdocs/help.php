de9246d16b1abf5d9392a8f592acf61cb8660a4d
Curtis Gedak <gedakc@gmail.com>
2011-12-17 16:01:01 -0700

    Create help navigation and summary page

---
+++ htdocs/help.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<?
+include "functions.php";
+?>
+
+<head>
+	<title>GParted -- Help</title>
+<? html_head() ?>
+</head>
+<body>
+<div class="body">
+
+<?
+gnome_head();
+gnome_menu();
+?>
+<div class="content">
+<h1>Help</h1>
+
+<p>
+Several different methods are available to learn about or receive help
+with GParted:
+<p>
+
+<ul>
+  <li class="step">
+    The <a href="documentation.php">documentation</a> page contains
+    in-depth information about GParted.
+  </li>
+  <li class="step">
+    The <a href="faq.php">FAQ</a> page lists answers to many commonly
+    asked questions.
+  </li>
+  <li class="step">
+    The <a href="forum.php">forum</a> is the first place to go to
+    receive assistance with problems.
+  </li>
+  <li class="step">
+    The <a href="bugs.php">bugs</a> page contains links to view and
+    report software bugs.
+  </li>
+  <li class="step">
+    The <a href="contact.php">contact</a> page has additional
+    communication methods.
+  </li>
+</ul>
+
+<p>
+To learn how to help the GParted project please see
+the <a href="howtohelp.php">how to help</a> page.
+</p>
+
+</div>
+
+<? gnome_foot() ?>
+
+</div>
+</body>
+</html>
