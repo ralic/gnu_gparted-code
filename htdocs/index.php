1ac4ea784c4b2c9839a0cca8a3255af5325cbd95
Steven Shiau <steven@nchc.org.tw>
2011-12-21 11:39:53 +0800

    Updated GParted live stable as 0.11.0-7.

--- htdocs/index.php
+++ htdocs/index.php
@@ -18,7 +18,7 @@ gnome_menu();
 <div class="rightbox">
 	<b>Latest release:</b><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.11.0/">GParted 0.11.0</a><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.11.0-2/">Live 0.11.0-2</a>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.11.0-7/">Live 0.11.0-7</a>
 	</div>
 <div class="content">
 
