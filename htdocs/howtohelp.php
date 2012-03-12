1e6ad76c153c560f1435e880b277e267f4af73a5
Curtis Gedak <gedakc@gmail.com>
2012-03-12 13:45:35 -0600

    Add more ways to help and re-word sections of how to help page

--- htdocs/howtohelp.php
+++ htdocs/howtohelp.php
@@ -17,24 +17,53 @@ gnome_menu();
 ?>
 <div class="content">
 <h1>How to help</h1>
-<h3>Bugs</h3>
+
+<p>You can help the GParted project and all the people that use
+GParted in many different ways:</p>
+
+<h3>Share</h3>
 <p>
-Properly filed bugs are a great help while developing software.<br>
-The bugs page can be found <a href="bugs.php">here</a>.
+Share GParted with friends and family.<br>
+More people using GParted means more perspectives, opinions, and ideas
+for us to learn how we can improve GParted for everyone.
 </p>
-<h3>Forum</h3>
+
+<h3>Publish</h3>
 <p>
-The forums are the first place to ask and receive support. It would be great if users would help each other.<br>
+Publish your experiences with GParted.<br>
+Publishing how you solved partitioning challenges enables more users
+to overcome similar hurdles.
+</p>
+
+<h3>Participate in Forums</h3>
+<p>
+Participating in forums enables you to provide and receive help from
+other users.<br>
+The forums are the first place to ask and receive support.<br>
 This is also the place to ask for and discuss new features.<br>
-Sign up and join the fun <a href="forum.php">here</a>.
+Sign up and join the fun in the <a href="forum.php">forum</a>.
 </p>
-<h3>Language Translation</h3>
+
+<h3>Report Bugs</h3>
+<p>
+Report bugs that you encounter.<br>
+Properly filed bugs are a great help while developing software and
+enable us to improve the quality of the software.<br>
+See the <a href="bugs.php">bugs</a> page for viewing and reporting bugs.
+</p>
+
+<h3>Translate Language</h3>
 <p>
 Translating GParted to your language is a valuable task that enables even more people to comfortably use GParted.<br>
-Learn more about the GNOME Translation Project <a href="http://live.gnome.org/TranslationProject/">here</a>.<br>
-View the status of GParted language translation <a href="http://l10n.gnome.org/module/gparted/">here</a>.
+Learn more about
+the <a href="http://live.gnome.org/TranslationProject/">GNOME
+Translation Project</a> which manages language translation for GNOME
+applications like GParted.<br>
+
+View also the status of <a href="http://l10n.gnome.org/module/gparted/">GParted language translation</a>.
 </p>
-<h3>Testing</h3>
+
+<h3>Test</h3>
 <p>
 Testing the functionality of GParted,
 reporting <a href="bugs.php">bugs</a>, and discussing problems is a
@@ -43,7 +72,8 @@ take place in
 the <a href="http://gparted-forum.surf4.info/viewforum.php?id=8">Testers</a>
 forum.
 </p>
-<h3>Coding</h3>
+
+<h3>Develop Patches</h3>
 <p>
 Fixing <a href="bugs.php">bugs</a>, implementing <a href="http://gparted-forum.surf4.info/viewforum.php?id=5">new features</a>,
 or addressing 'FIXME' and 'TODO' comments in the code can be great
@@ -55,7 +85,8 @@ before embarking on significant code changes.<br>
 <br>
 See also the <a href="development.php">development</a> page.
 </p>
-<h3>Donations</h3>
+
+<h3>Make a Donation</h3>
 <p>
 Another big way to help us to make GParted better is to make a
 donation to the GParted project.  For details on donations please
