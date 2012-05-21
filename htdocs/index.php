21839ddc5f8e4004c11849e639df02eed216ed2c
Steven Shiau <steven@nchc.org.tw>
2012-05-21 23:59:15 +0800

    Updating gparted live stable as 0.12.1-5.

--- htdocs/index.php
+++ htdocs/index.php
@@ -18,7 +18,7 @@ gnome_menu();
 <div class="rightbox">
 	<b>Latest release:</b><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.12.1/">GParted 0.12.1</a><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.12.1-1/">Live 0.12.1-1</a>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.12.1-5/">Live 0.12.1-5</a>
 	</div>
 <div class="content">
 
