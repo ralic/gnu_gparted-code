cbeb85b849b0686906da9fad9a8c0fd9e98770de
Steven Shiau <steven@nchc.org.tw>
2012-06-01 12:12:56 +0800

    Updating for current creating environment.

--- htdocs/create-gparted-live.php
+++ htdocs/create-gparted-live.php
@@ -21,21 +21,24 @@ gnome_menu();
 <b>GParted Live</b> is a small bootable GNU/Linux distribution for x86 machine based on Debian live. If you want to create GParted live from scratch, you can follow the following:<br>
 <br>
 <ol>
-  <li class="step">Boot GParted live (Version 0.6.0-6 or later is required).</li>
-  <li class="step">Follow the instructions to enter X, then open a terminal.</li>
+  <li class="step">Boot GParted live (Version <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/OldFiles/0.9.1-1/" target=_blank>0.9.1-1</a> is required. Do not use newer version due to we are still using live-build 2.x around Jun/2012).</li>
+  <li class="step">Follow the instructions to enter X, then open a terminal. The follow actions have to be run as root. If you are not root, run "sudo -i" to become root.</li>
   <li class="step">Configure internet connection, e.g. run "dhclient eth0" to lease IP address from a DHCP server, or you can use commands like "ifconfig" and "route" to assign static IP address.</li>
-  <li class="step">apt-get update</li>
   <li class="step">Turn on the apt repository by editing /etc/apt/sources.list, e.g. "nano /etc/apt/sources.list" or "vim /etc/apt/sources.list", make it like:
 <pre>
 deb http://ftp.us.debian.org/debian/ sid main contrib non-free # Or any Debian mirror site near you.
 </pre>
   </li>
   <li class="step">apt-get update</li>
+  <li class="step">apt-get install apt</li>
+  <li class="step">apt-get update</li>
   <li class="step">mkdir -p /usr/share/man/man{1,5,8}</li>
   <li class="step">apt-get --reinstall install libc6<br>
     (By doing this, we can avoid this error: "iconv: conversion to `latin1' is not supported" when creating GParted live by live helper because /usr/lib/gconv/ was removed when GParted live was created)
   </li>
-  <li class="step">apt-get install live-helper cdebootstrap drbl clonezilla zip bzip2 rsync mkisofs</li>
+  <li class="step">apt-get install live-build=2.0.12-2.drbl2 cdebootstrap debootstrap drbl clonezilla zip bzip2 rsync genisoimage binutils
+  <li>export PATH=$PATH:/opt/drbl/sbin/:/opt/drbl/bin/</li>
+  <li class="step">run command "drbl-prepare-pxelinux"
   <li class="step">Mount a working space, you need at least 600 MB. e.g. mount /dev/sdb1 /mnt</li>
   <li class="step">cd /mnt</li>
   <li class="step">Edit /opt/drbl/sbin/create-gparted-live to meet your need if necessary, e.g. if you want to add some package, you can append the package name in the variable "pkgs" if it exists in the Debian repository. E.g. if you want to add the package "pcmanfm", you can try to search it in the Debian repository by: "apt-cache search pcmanfm". Once you are sure it's in repository, you can add it.</li>
@@ -46,8 +49,9 @@ e.g. to use the unstable branch of DRBL and experimental branch of live on drbl 
 If you want to assign different repositories to download files, you can run something like:<br>
 /opt/drbl/sbin/create-gparted-live -l en -b u -e e -m http://ftp.us.debian.org/debian -s http://security.debian.org -g http://drbl.sourceforge.net/drbl-core<br>
   <br>
-Or more options you can try:<br>
-/opt/drbl/sbin/create-gparted-live -c 'main non-free' -k 'firmware-linux-free firmware-linux firmware-linux-nonfree firmware-bnx2 firmware-bnx2x firmware-qlogic firmware-ralink mlterm=2.9.4-6 mlterm-common=2.9.4-6' -l en -b u -e e -d sid -m http://ftp.us.debian.org/debian -s http://security.debian.org -g http://free.nchc.org.tw/drbl-core -x 'i915.modeset=0 xforcevesa' -n 2.6.32-5 -i my-gparted-live<br>
+Or more options you can try (this is how GParted live 0.12.1-5 was created):<br>
+create-gparted-live -bt debootstrap -c main non-free -k firmware-linux-free firmware-linux firmware-linux-nonfree firmware-bnx2 firmware-bnx2x firmware-qlogic firmware-ralink live-boot=2.0.15-1.drbl13 live-boot-initramfs-tools=2.0.15-1.drbl13 live-config=2.0.15-1.drbl6 live-config-sysvinit=2.0.15-1.drbl6 partimage cifs-utils=2:5.3-2 -l en -b u -e e -d sid -m http://free.nchc.org.tw/debian -s http://free.nchc.org.tw/debian-security -g http://free.nchc.org.tw/drbl-core -n 3.2.0-2 -i 0.12.1-5
+  <br>
   <br>
 For more info, you can run "/opt/drbl/sbin/create-gparted-live --help"<br>
 BTW, in the file "GParted-Live-Version" in the GParted live iso file, you can find the command which was used to create the GParted iso file.
