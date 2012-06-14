34bed138402758d5cd15ae79055ed800d3e47106
Curtis Gedak <gedakc@gmail.com>
2012-06-14 16:52:46 -0600

    Add new articles page with links to articles featuring GParted

---
+++ htdocs/articles.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<?
+include "functions.php";
+?>
+
+<head>
+	<title>GParted -- Articles</title>
+<? html_head() ?>
+</head>
+<body>
+<div class="body">
+
+<?
+gnome_head();
+gnome_menu();
+?>
+<div class="content">
+<h1>Articles Featuring GParted</h1>
+
+<ul>
+  <li class="step">
+    <a href="http://www.makeuseof.com/pages/best-linux-distributions">
+    Best Linux Distributions</a><br>
+    by makeuseof on January 2012<br>
+    Type:  Rating<br>
+    <a href="http://www.makeuseof.com/pages/best-linux-distributions" title="Best Linux Distros"><img src="http://makeuseof.com/images/logo/reviewed.png" alt="Best Linux Distros"></a><br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.pcworld.com/article/226231/gparted_live_a_boot_disk_iso_you_cant_afford_to_be_without.html">
+    GParted Live: A Boot Disk ISO You Can't Afford to Be Without</a><br>
+    by Jon L. Jacobi on May 1, 2011<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.makeuseof.com/tag/gparted-ultimate-partitioning-software/">
+    GParted – The Ultimate In Partitioning Software</a><br>
+    by Justin Pot on July 22, 2010<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://sourceforge.net/blog/gparted-is-such-sweet-software/">
+    GParted is such sweet software</a><br>
+    by Lee Schlesinger on March 19, 2010<br>
+    Type:  Interview<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://helpdeskgeek.com/free-tools-review/manage-partitions-in-windows-7-xp-vista/">
+    Use GParted to Create, Delete, Copy or Resize Partitions</a><br>
+    by Aseem on January 4, 2010<br>
+    Type:  Tutorial<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.syschat.com/gparted-disk-partition-manager-editor-review-5073.html">
+    GParted Disk Partition Manager/Editor Review</a><br>
+    by Sami on October 28, 2009<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.dedoimedo.com/computers/gparted.html">
+    GParted partitioning software - Full tutorial</a><br>
+    by Dedoimedo, updated on April 25, 2009<br>
+    Type:  Tutorial<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://opcug.ca/public/reviews/gparted.htm">
+    GParted – A little open-source “partition magic”</a><br>
+    by Alan German October 27, 2008<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://archive09.linux.com/feature/147591">
+    A comparative look at compact sysadmin distributions</a><br>
+    by Cory Buford on September 17, 2008<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.linuxhaxor.net/7-uses-of-gparted-live/">
+    7 Uses of GParted Live</a><br>
+    by Gary on August 2, 2008<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.computeractive.co.uk/ca/download-review/1916751/gnome-partition-editor-039-live-cd">
+    Gnome Partition Editor 0.3.9-4 Live CD</a><br>
+    by Tim Smith on February 10, 2008<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.howtogeek.com/howto/windows-vista/using-gparted-to-resize-your-windows-vista-partition/">
+    Using GParted to Resize Your Windows 7 or Vista Partition</a><br>
+    by The Geek on August 8, 2007<br>
+    Type:  Tutorial<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://blogcritics.org/scitech/article/non-destructive-partitioning-with-gparted/">
+    Non-Destructive Partitioning with GParted</a><br>
+    by Steve Wild on April 01, 2007<br>
+    Type:  Tutorial<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://techgage.com/article/gparted_livecd_31-1">
+    GParted LiveCD 3.1-1</a><br>
+    by Rob Williams on September 15, 2006<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.reviewlinux.com/reviewlinux-gparted-livecd-0-2-5-1-a-must-have-disc-10.html#more-10">
+    GParted LiveCD 0.2.5-1 A Must Have Disc!</a><br>
+    by Michael Perks on June 17, 2006<br>
+    Type:  Review<br>
+    <br>
+  </li>
+  <li class="step">
+    <a href="http://www.softpedia.com/reviews/linux/GParted-Review-22766.shtml">
+    GParted Review</a><br>
+    by Bogdan Radulescu on May 4, 2006<br>
+    Type:  Review<br>
+    <a href="http://www.softpedia.com/reviews/linux/GParted-Review-22766.shtml#"    title="Softpedia Review Award">
+    <img src="images/softpedia_pick_award_s.gif" border="0"
+     alt="Softpedia Review Award Logo">
+    </a><br>
+    <br>
+  </li>
+</ul>
+
+</div>
+
+<? gnome_foot() ?>
+
+</div>
+</body>
+</html>
