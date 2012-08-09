dd03b8957ad561b8bbbb7583c682dc249f3305e0
Curtis Gedak <gedakc@gmail.com>
2012-08-09 16:11:33 -0600

    Announce GParted Live 0.13.1-2 stable release

--- htdocs/download.php
+++ htdocs/download.php
@@ -67,7 +67,7 @@ a donation to help us make GParted better.<br>
 
   <tr>
     <td>
-      <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.13.0-3/gparted-live-0.13.0-3.iso/download">Download latest gparted-live-0.13.0-3.iso</a><br>
+      <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.13.1-2/gparted-live-0.13.1-2.iso/download">Download latest gparted-live-0.13.1-2.iso</a><br>
       <br>
       <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/">Stable directory (.iso/.zip)</a>
     </td>
