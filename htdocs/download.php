c6fb76593c33913f7723a42ba06420a2533ad124
Steven Shiau <steven@nchc.org.tw>
2012-12-28 09:32:54 +0800

    Default live download URL was updated for 0.14.1-6.

--- htdocs/download.php
+++ htdocs/download.php
@@ -106,7 +106,7 @@ architectures
 
   <tr>
     <td>
-      <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.14.1-1/gparted-live-0.14.1-1.iso/download">Download latest gparted-live-0.14.1-1.iso</a><br>
+      <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/0.14.1-6/">Download latest gparted-live-0.14.1-6</a><br>
       <br>
       <a href="http://sourceforge.net/projects/gparted/files/gparted-live-stable/">Stable directory (.iso/.zip)</a>
     </td>
