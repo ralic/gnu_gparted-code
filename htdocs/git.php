afbe0095513972f5a11d494697d336035e25a348
Curtis Gedak <gedakc@gmail.com>
2014-01-07 10:39:09 -0700

    Add size units to git video description on git page

--- htdocs/git.php
+++ htdocs/git.php
@@ -319,7 +319,7 @@ On failure examine the log file testbuild.log for details.
   <li><a href="https://live.gnome.org/Git/Developers">GNOME git instructions</a></li>
   <li><a href="http://git-scm.com/">Git homepage</a></li>
   <li><a href="http://git-scm.com/book">Pro Git</a> online book</li>
-  <li><a href="http://mirror.linux.org.au/linux.conf.au/2013/mp4/Git_For_Ages_4_And_Up.mp4">Git For ages 4 and up</a> LCA 2013 video (342M, 1h40m)</li>
+  <li><a href="http://mirror.linux.org.au/linux.conf.au/2013/mp4/Git_For_Ages_4_And_Up.mp4">Git For ages 4 and up</a> LCA 2013 video (342MB, 1h40m)</li>
   <li><a href="http://gitolite.com/gcs/">Git concepts simplified</a></li>
 </ul>
 
