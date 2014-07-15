c612eea2f902e4b4940afe3af6d353e65989281a
Curtis Gedak <gedakc@gmail.com>
2014-07-15 10:16:01 -0600

    Update news and links for gparted 0.19.1 release

--- htdocs/index.php
+++ htdocs/index.php
@@ -19,7 +19,7 @@ gnome_menu();
 ?>
 <div class="rightbox">
 	<b>Stable release:</b><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.19.0/">GParted 0.19.0</a><br>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.19.1/">GParted 0.19.1</a><br>
 	<a href="http://downloads.sourceforge.net/gparted/gparted-live-0.19.0-1-i486.iso">Live 0.19.0-1</a>
 	</div>
 <div class="content">
