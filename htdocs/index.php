69b1bd27e3a75297723db14190c782abce3a7f02
Curtis Gedak <gedakc@gmail.com>
2016-01-18 10:20:41 -0700

    Update news and links for gparted 0.25.0 release

--- htdocs/index.php
+++ htdocs/index.php
@@ -19,7 +19,7 @@ gnome_menu();
 ?>
 <div class="rightbox">
 	<b>Stable release:</b><br>
-	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.24.0/">GParted 0.24.0</a><br>
+	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.25.0/">GParted 0.25.0</a><br>
 	<a href="http://downloads.sourceforge.net/gparted/gparted-live-0.24.0-2-i586.iso">Live 0.24.0-2</a>
 	</div>
 <div class="content">
