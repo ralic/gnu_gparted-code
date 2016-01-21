ee5821d1d37837fbfd3bb789e90629029ae4021f
Steven Shiau <steven@nchc.org.tw>
2016-01-21 08:01:56 +0800

    index.php: stable release 0.25.0-1-i686
    Make stable release as 0.25.0-1-i686.

--- htdocs/index.php
+++ htdocs/index.php
@@ -20,7 +20,7 @@ gnome_menu();
 <div class="rightbox">
 	<b>Stable release:</b><br>
 	<a href="http://sourceforge.net/projects/gparted/files/gparted/gparted-0.25.0/">GParted 0.25.0</a><br>
-	<a href="http://downloads.sourceforge.net/gparted/gparted-live-0.24.0-2-i586.iso">Live 0.24.0-2</a>
+	<a href="http://downloads.sourceforge.net/gparted/gparted-live-0.25.0-1-i686.iso">Live 0.25.0-1</a>
 	</div>
 <div class="content">
 
