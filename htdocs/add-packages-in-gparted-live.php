f2f8fc01bac7fe331ed121f6baefa3cf51037e08
Curtis Gedak <gedakc@gmail.com>
2017-05-14 09:33:13 -0600

    Minor changes to pass HTML validation for add-packages-to-gparted-live
    The following changes were made to enable this page to pass HTML
    validation:
    
    - Convert <pre> tags to <code> tags.
    - Add <br> tags for new lines to simulate <pre> behaviour
    - Add </li> end tags which encompass <code> tags

--- htdocs/add-packages-in-gparted-live.php
+++ htdocs/add-packages-in-gparted-live.php
@@ -22,42 +22,56 @@ gnome_menu();
  <font color="red">//NOTE// Due to the changes in Debian Sid repository are very frequent, the following method might need to be adjusted according to the changes.</font>
 <br>
 <ol>
-  <li>You need a GNU/Linux system as a working environment to do this. Here we use Debian Jessie as the working environment. First we need to install squashfs-tools if it's not installed:
-  <pre>
-  $ sudo apt-get update; sudo apt-get install squashfs-tools
-  </pre>
-  <li class="step">Download the GParted live zip format file which you intend to add packages, e.g., GParted live version <a href="http://downloads.sourceforge.net/gparted/gparted-live-0.28.1-1-i686-pae.zip" target=_blank>0.28.1-1</a>, then uncompress it and extract the filesystem.squashfs</li>
-  <pre>
-  $ mkdir ~/zip-tmp ~/squashfs-tmp
-  $ unzip gparted-live-0.28.1-1-i686-pae.zip -d ~/zip-tmp
-  $ cp ~/zip-tmp/live/filesystem.squashfs ~/squashfs-tmp
-  $ cd ~/squashfs-tmp; sudo unsquashfs filesystem.squashfs
-  (it will extract filesystem.squashfs to directory "squashfs-root" )
-  </pre>
-  <li>Assume the package you intend to add exists in Debian repository, e.g., firefox-esr, then we can do it by:
-  <pre>
-  $ sudo mount --bind /proc squashfs-root/proc/
-  $ sudo mount --bind /sys squashfs-root/sys/
-  $ sudo mount --bind /dev squashfs-root/dev/
-  $ sudo chroot squashfs-root/
-  $ echo "nameserver 8.8.8.8" > /etc/resolv.conf
-  (It will write the nameserver for the chroot environment. You can use your desired namer server)
-  $ apt-get update; apt-get install firefox-esr
-  $ exit
-  </pre>
-  <li>Rebuild the new filesystem.squashfs and replace the original one ,then rebuild GParted live zip file with a XZ-compressed filesystem.squashfs:
-  <pre>
-  $ sudo mksquashfs squashfs-root filesystem.squashfs.new -b 1024k -comp xz -Xbcj x86 -e boot
-  $ sudo cp filesystem.squashfs.new ~/zip-tmp/live/filesystem.squashfs
-  $ cd ~/zip-tmp ; sudo zip -r ../gparted-live.new.zip ./*
-  </pre>
-  Now you have gparted-live.new.zip with firefox-esr included.
+  <li>You need a GNU/Linux system as a working environment to do this. Here we use Debian Jessie as the working environment. First we need to install squashfs-tools if it's not installed:<br>
   <br>
-  <li>If you want to convert zip file to iso file, you can make it by:
-  <pre>
-  $ cd ~/zip-tmp/
-  $ genisoimage -A 'GParted live CD' -f -r -hide-rr-moved -hide-joliet-trans-tbl -J -l -allow-limited-size -b syslinux/isolinux.bin -c syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -efi-boot EFI/images/efiboot.img -no-emul-boot ./ > /tmp/gparted-live.iso 
-  </pre>
+  <code>
+  $ sudo apt-get update; sudo apt-get install squashfs-tools<br>
+  </code>
+  <br>
+  </li>
+  <li class="step">Download the GParted live zip format file which you intend to add packages, e.g., GParted live version <a href="http://downloads.sourceforge.net/gparted/gparted-live-0.28.1-1-i686-pae.zip" target=_blank>0.28.1-1</a>, then uncompress it and extract the filesystem.squashfs<br>
+  <br>
+  <code>
+  $ mkdir ~/zip-tmp ~/squashfs-tmp<br>
+  $ unzip gparted-live-0.28.1-1-i686-pae.zip -d ~/zip-tmp<br>
+  $ cp ~/zip-tmp/live/filesystem.squashfs ~/squashfs-tmp<br>
+  $ cd ~/squashfs-tmp; sudo unsquashfs filesystem.squashfs<br>
+  (it will extract filesystem.squashfs to directory "squashfs-root" )<br>
+  </code>
+  <br>
+  </li>
+  <li>Assume the package you intend to add exists in Debian repository, e.g., firefox-esr, then we can do it by:<br>
+  <br>
+  <code>
+  $ sudo mount --bind /proc squashfs-root/proc/<br>
+  $ sudo mount --bind /sys squashfs-root/sys/<br>
+  $ sudo mount --bind /dev squashfs-root/dev/<br>
+  $ sudo chroot squashfs-root/<br>
+  $ echo "nameserver 8.8.8.8" > /etc/resolv.conf<br>
+  (It will write the nameserver for the chroot environment. You can use your desired namer server)<br>
+  $ apt-get update; apt-get install firefox-esr<br>
+  $ exit<br>
+  </code>
+  <br>
+  </li>
+  <li>Rebuild the new filesystem.squashfs and replace the original one ,then rebuild GParted live zip file with a XZ-compressed filesystem.squashfs:<br>
+  <br>
+  <code>
+  $ sudo mksquashfs squashfs-root filesystem.squashfs.new -b 1024k -comp xz -Xbcj x86 -e boot<br>
+  $ sudo cp filesystem.squashfs.new ~/zip-tmp/live/filesystem.squashfs<br>
+  $ cd ~/zip-tmp ; sudo zip -r ../gparted-live.new.zip ./*<br>
+  </code>
+  <br>
+  Now you have gparted-live.new.zip with firefox-esr included.<br>
+  <br>
+  </li>
+  <li>If you want to convert zip file to iso file, you can make it by:<br>
+  <br>
+  <code>
+  $ cd ~/zip-tmp/<br>
+  $ genisoimage -A 'GParted live CD' -f -r -hide-rr-moved -hide-joliet-trans-tbl -J -l -allow-limited-size -b syslinux/isolinux.bin -c syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -efi-boot EFI/images/efiboot.img -no-emul-boot ./ > /tmp/gparted-live.iso<br>
+  </code>
+  </li>
 </ol>
 
 </div>
